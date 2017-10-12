1a869797269999a151e3268df5fd45af3ce53574
N. Venkat Raj <venkat@codinggeek.com>
2017-10-12 16:56:57 +0530

    v.1.0.6

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -38,18 +38,19 @@ if ( ! function_exists( 'magzen_header_style' ) ) :
  * @see magzen_custom_header_setup().  
  */
 function magzen_header_style() {
-	if ( get_header_image() ) {
-	?>
-	<style type="text/css">    
-		.header-image {
-			background-image: url(<?php echo esc_url(get_header_image()); ?>);
-			display: block;
-		}   
-		.custom-header-media img {
-				display: none;
-		}   
-	</style>
-	<?php
+  
+	// If we get this far, we have custom styles. Let's do this.
+	
+	if ( get_header_image() ) { ?>
+		<style type="text/css">
+			.header-image {
+				background-image: url(<?php echo esc_url(get_header_image()); ?>);
+				display: block;
+			}   
+			.custom-header-media img {
+					display: none;
+			}   
+		</style><?php
 	}
    /* Header Video Settings */
     if(function_exists('is_header_video_active') ) {
@@ -76,6 +77,36 @@ function magzen_header_style() {
 		    </style><?php
 		}
     }
+
+     $header_text_color = get_header_textcolor();
+
+	/*
+	 * If no custom options for text are set, let's bail.
+	 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
+	 */
+	if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
+		return;
+	} ?>
+    <style type="text/css">   
+	     <?php
+			// Has the text been hidden?
+			if ( ! display_header_text() ) :
+		?>
+			.site-title,
+			.site-description {
+				position: absolute;
+				clip: rect(1px, 1px, 1px, 1px);
+			}
+		<?php
+			// If the user has set a custom color for the text use that.
+			else :
+		?>
+			.branding .site-branding .site-title a,
+			.site-description {
+				color: #<?php echo esc_attr( $header_text_color ); ?>;
+			}
+		<?php endif; ?> 
+	</style><?php
 }
 endif; // magzen_header_style
 
